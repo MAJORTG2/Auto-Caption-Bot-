76a86cc8ff2ba2dcd57141519e6c65b0794a5585
MAJORTG <99739229+MAJORTG2@users.noreply.github.com>
2022-09-09 20:53:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
 try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
-AutoCaptionBotV = pyrogram.Client(
-   name="AutoCaptionBotV", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
+AutoCaptionBotV1 = pyrogram.Client(
+   name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
 
 start_message = """
 <b>👋Hello {}</b>
@@ -26,27 +26,27 @@ about_message = """
 <b>• Updates : <a href=https://t.me/movie_time_botonly>Click Here</a></b>
 <b>• Source Code : <a href=https://github.com/MAJORTG2/Auto-Caption-Bot->Click Here</a></b>"""
 
-@AutoCaptionBotV.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
+@AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
 def start_command(bot, update):
   update.reply(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
-@AutoCaptionBotV.on_callback_query(pyrogram.filters.regex("start"))
+@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("start"))
 def strat_callback(bot, update):
   update.message.edit(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
-@AutoCaptionBotV.on_callback_query(pyrogram.filters.regex("about"))
+@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("about"))
 def about_callback(bot, update): 
   bot = bot.get_me()
   update.message.edit(about_message.format(version=pyrogram.__version__, username=bot.mention), reply_markup=about_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
-@AutoCaptionBotV.on_message(pyrogram.filters.channel)
+@AutoCaptionBotV1.on_message(pyrogram.filters.channel)
 def edit_caption(bot, update: pyrogram.types.Message):
   motech, _ = get_file_details(update)
   try:
       try: update.edit(custom_caption.format(file_name=motech.file_name))
       except pyrogram.errors.FloodWait as FloodWait:
           asyncio.sleep(FloodWait.value)
-          update.edit(custom_caption.format(file_name=motech.file_name))
+          update.edit(custom_caption.format(file_name=trumbots.file_name))
   except pyrogram.errors.MessageNotModified: pass 
     
 def get_file_details(update: pyrogram.types.Message):
@@ -86,7 +86,7 @@ def about_buttons(bot, update):
    ]]
   return pyrogram.types.InlineKeyboardMarkup(buttons)
 
-print("Telegram AutoCaption V Bot Start")
+print("Telegram AutoCaption V1 Bot Start")
 print("Bot Created By https://github.com/MAJORTG2/Auto-Caption-Bot-")
 
-AutoCaptionBotV.run()
+AutoCaptionBotV1.run()
