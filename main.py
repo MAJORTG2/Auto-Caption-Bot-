2e6a604826ff5efdd46903dae80c21e0a62337e5
MAJORTG <99739229+MAJORTG2@users.noreply.github.com>
2022-09-10 21:16:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,6 +77,8 @@ def start_buttons(bot, update):
    pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
    ],[
    pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
+   ]],[
+   pyrogram.types.InlineKeyboardButton("SOURCE CODE", url="https://github.com/MAJORTG2/Auto-Caption-Bot-")
    ]]
   return pyrogram.types.InlineKeyboardMarkup(buttons)
 
